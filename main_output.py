f24f7e7478ebd602aa2312a38a3f14db8038afb1
Marsus <202110565@feualabang.edu.ph>
2023-05-12 16:22:28 +0800

    main output

---
+++ main_output.py
@@ -0,0 +1,16 @@
+Please enter the pasaload amount (Type 69 to exit): 
+555
+Please enter the pasaload amount (Type 69 to exit): 
+444
+Please enter the pasaload amount (Type 69 to exit): 
+333
+Please enter the pasaload amount (Type 69 to exit): 
+69
+PASALOAD: Successfully loaded 555 pesos. New balance: 1555 pesos.
+PASALOAD: Successfully loaded 444 pesos. New balance: 1999 pesos.
+PASALOAD: Successfully loaded 333 pesos. New balance: 2332 pesos.
+I repeat all pasaload are complete!.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
